981e6feb4eb7e5f70e27e7f415451456e819b6d4
MOON <78653601+kamoo2@users.noreply.github.com>
2023-11-15 02:09:29 +0000

    선택 정렬 알고리즘

---
+++ Algorithm/Sort/SelectSort.js
@@ -0,0 +1,30 @@
+// 첫번째값을 두번째 값부터 끝까지 비교하면서 최소값 찾고 첫번째에 위치 시킴
+// 두번째 값을 세번째 값부터 비교해서 최소값 찾고 두번째에 위치 시킴
+// 마지막까지 반복
+// 선택정렬은 첫 번째 값을 두번째 값부터 끝까지 비교하면서 최소값 찾고 첫번째에 위치시키고
+// 두번째 값을 세번째 값부터 마지막까지 비교하면서 최소값 찾고 두번째에 위치시키는 과정을 반복하면서 정렬하는 알고리즘 입니다.
+// 시간 복잡도는 O(n^2)입니다.
+
+function selectSort(arr) {
+    let n = arr.length;
+  
+    for (let i = 0; i < n - 1; i++) {
+      // 최소값을 찾기 위한 변수 min을 현재 인덱스로 초기화
+      let min = i;
+      for (let j = i + 1; j < n; j++) {
+        if (arr[j] < arr[min]) {
+          min = j;
+        }
+      }
+  
+      if (min !== i) {
+        [arr[min], arr[i]] = [arr[i], arr[min]];
+      }
+    }
+    return arr;
+  }
+  
+  
+const data = [6,3,9,3,2,7,6,4,1];
+
+console.log(`Select Sort : ${selectSort(data)}`)
